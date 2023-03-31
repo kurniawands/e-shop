18636ecf936284155c791d3fd125db6c35b72cf8
Kurniawan DS <kds.wawan@gmail.com>
2023-04-01 00:16:28 +0700

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-1": "#1c1c18",
+        "custom-2": "#cfb47d",
+        "custom-3": "#f3be5c",
+        "custom-4": "#a3772f",
+        "custom-5": "#fffefb",
+      },
+    },
   },
   plugins: [],
-}
+};
