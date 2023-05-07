db2fb172a2e81446a03d1fc985c48a8387cc2a5b
Kurniawan DS <kds.wawan@gmail.com>
2023-05-07 19:39:54 +0700

    change mapping

--- components/products/Box.jsx
+++ components/products/Box.jsx
@@ -5,21 +5,21 @@ export default function Box({ items }) {
   return (
     <div className="grid grid-cols-3 gap-5">
       {items.map((item) => (
-        <Link key={item.ID} href="/">
+        <Link key={item.prodid} href="/">
           <div className="px-3 py-7 border rounded-xl">
             <div className="flex flex-col h-64">
               <div className="h-40">
                 <Image
                   className="max-h-full mx-auto object-contain"
-                  src={item.Path}
+                  src={item.prodimgpath}
                   width={300}
                   height={300}
                 />
               </div>
               <div className="flex flex-col h-full justify-between">
-                <div className="text-lg text-custom-4">{item.Name}</div>
+                <div className="text-lg text-custom-4">{item.prodname}</div>
                 <div className="font-semibold text-[#4a4a4a]">
-                  Rp. {item.Price}
+                  Rp. {item.prodprice}
                 </div>
                 <div className="flex flex-row space-x-2">
                   <Image src="/rating.svg" width={15} height={15} />
