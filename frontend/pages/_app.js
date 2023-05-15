65b6f32ed08fd70d91c96aadc6c1b590a7b3917e
Kurniawan DS <kds.wawan@gmail.com>
2023-05-15 14:58:43 +0700

    delete frontend duplicate

--- frontend/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
